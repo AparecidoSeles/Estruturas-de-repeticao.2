204b735349b10a0f127a139dc1d571da67f6f681
junior <you@example.com>
2020-12-28 18:31:38 -0300

    estruturas de repetição exerc 2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Estruturas_de_repeticao._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+            //Atributos 
+            string nomeUsuario;
+            string senhaUsuario;
+
+            Console.WriteLine("Insira um nome de usuário");
+            nomeUsuario  = Console.ReadLine();
+
+            Console.WriteLine("Insira a senha do usuário");
+            senhaUsuario = Console.ReadLine();
+            
+        }
+    }
+}
